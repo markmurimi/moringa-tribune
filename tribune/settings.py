36538317b20eb6a241a6b7361e8b380ce563be00
mark <murimimg180@gmail.com>
2018-05-17 16:18:44 +0300

    made changes

--- tribune/settings.py
+++ tribune/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'news.apps.NewsConfig',
     'bootstrap3',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
