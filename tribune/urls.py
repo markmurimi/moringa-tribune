0291908237f5d2674cc27c93b77b93cfac7e4996
mark <murimimg180@gmail.com>
2018-05-18 08:14:33 +0300

    made changes

--- tribune/urls.py
+++ tribune/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url,include
 from django.contrib import admin
-from django.contrib.auth import views 
+from django.contrib.auth import views
+from rest_framework.authtoken.views import obtain_auth_token
+
 
 urlpatterns = [
+    url(r'^api-token-auth/', obtain_auth_token),
     url(r'^admin/', admin.site.urls),
     url(r'',include('news.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
